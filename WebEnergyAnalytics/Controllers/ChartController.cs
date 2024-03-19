485c56db52121a192279eee0aa3f9fb1bba986e0
Mikhail <mihail_9000@mail.ru>
2024-03-19 13:51:45 +0300

    Добавил подключение к бд и вывод данных.

--- WebEnergyAnalytics/Controllers/ChartController.cs
+++ WebEnergyAnalytics/Controllers/ChartController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace WebEnergyAnalytics.Controllers
 {
     public class ChartController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public ChartController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Получаем строку подключения из файла json
+        /// </summary>
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
         public IActionResult Index()
         {
-            return View();
+            var model = GetData();
+            return View(model);
+        }
+
+        public class Data
+        {
+            public string Name { get;}
+            public DateTime Date { get;}
+            public double Value { get;}
+        }
+
+        private List<Data> GetData()
+        {
+            using(IDbConnection db = Connection)
+            {
+                var result = db.Query<Data>("SELECT * FROM my_test_db WHERE name='T_obr_1'").ToList();
+
+                return result;
+            }
         }
     }
 }
