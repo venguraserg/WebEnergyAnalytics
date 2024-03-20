5a1ad717769cc4cab6c1ed1e23a3c648682ac94b
Mikhail <mihail_9000@mail.ru>
2024-03-20 09:10:27 +0300

    ...

--- WebEnergyAnalytics/Controllers/ChartController.cs
+++ WebEnergyAnalytics/Controllers/ChartController.cs
@@ -41,18 +41,9 @@ namespace WebEnergyAnalytics.Controllers
         {
             try
             {
-                using (IDbConnection db = Connection)
-                {
-                    var result = db.Query<Data>("SELECT * FROM my_test_db WHERE name='T_obr_1'").ToList();
-                    var dict = new Dictionary<DateTime, double>();
+                var result = db.Query<Data>("SELECT * FROM my_test_db WHERE name='T_obr_1'").ToList();
 
-                    foreach (var item in result)
-                    {
-                        dict[item.Date] = item.Value;
-                    }
-                    //var sortedDict = new SortedDictionary<DateTime, double>(dict);
-                    return dict;
-                }
+                return result;
             }
             catch { return null; }
         }
